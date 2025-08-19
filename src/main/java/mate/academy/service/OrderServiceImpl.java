26f1f56661de8d4115fd5d8ac07f48213a337be2
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-08-19 02:27:06 +0200

    logger dummy data))

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -28,7 +28,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data was successfully fetched from DB for userId: {}", userId);
+        logger.info("Returning dummy product list for userId: {}", userId);
         return products;
     }
 }
